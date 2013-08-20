9ba2270af6b29b3f4ab24a7444e09d713213f0c0
Tsanko Tsolov <tsanko.tsolov@gmail.com>
2013-08-20 11:51:13 -0700

    Simplified (and seemless) placeholder setting.

--- placeholder.js
+++ placeholder.js
@@ -1,22 +1,24 @@
 app.directive('ngPlaceholder', function () {
 
-    var i = document.createElement('input');
-    if ('placeholder' in i) {
-        return {};
-    }
-
     return {
         restrict: 'A',
         require: 'ngModel',
         // Don't isolate the scope as this directive modifies DOM outside the element's scope.
         //scope: {},
         compile: function (element, attrs) {
+            
+            var i = document.createElement('input');
+            if ('placeholder' in i) {
+                // yay! placeholder is supported - set it!
+                element.attr('placeholder', attrs.zbxPlaceholder);
+                return {};
+            }
 
             element.parent().append('<div class="placeholder" ng-click="setFocusFor(\'' + attrs.ngModel + '\')">{{ placeholder' + attrs.ngModel + ' }}</div>');
 
             return function (scope) {
 
-                scope.$parent['placeholder' + attrs.ngModel] = element.attr('placeholder');
+                scope.$parent['placeholder' + attrs.ngModel] = attrs.zbxPlaceholder;
 
                 scope.$parent.setFocusFor = function (model) {
                     angular.element('input[ng-model=' + model + ']').focus();
@@ -24,13 +26,13 @@ app.directive('ngPlaceholder', function () {
 
                 element.on('keyup', function () {
                     scope.$apply(function () {
-                        scope.$parent['placeholder' + attrs.ngModel] = (element.val() === '') ? element.attr('placeholder') : scope.$parent['placeholder' + attrs.ngModel] = '';
+                        scope.$parent['placeholder' + attrs.ngModel] = (element.val() === '') ? attrs.zbxPlaceholder : scope.$parent['placeholder' + attrs.ngModel] = '';
                     });
 
                 }).on('blur', function () {
                         if (element.val() === '') {
                             scope.$apply(function () {
-                                scope.$parent['placeholder' + attrs.ngModel] = element.attr('placeholder');
+                                scope.$parent['placeholder' + attrs.ngModel] = attrs.zbxPlaceholder;
                             });
                         }
                     });
