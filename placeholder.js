8d394254b1882bd458e8ddfb5240da7bdd2acaa4
Tsanko Tsolov <tsanko.tsolov@gmail.com>
2013-11-05 11:09:02 -0800

    Update placeholder.js

--- placeholder.js
+++ placeholder.js
@@ -7,35 +7,76 @@ app.directive('ngPlaceholder', function () {
         //scope: {},
         compile: function (element, attrs) {
             
-            var i = document.createElement('input');
-            if ('placeholder' in i) {
-                // yay! placeholder is supported - set it!
-                element.attr('placeholder', attrs.zbxPlaceholder);
-                return {};
-            }
+    		// --Use this if regular placeholder is used (except for IE9)
+            // var i = document.createElement('input');
+            // if ('placeholder' in i) {
+            //     // yay! placeholder is supported - set it!
+            //     element.attr('placeholder', attrs.zbxPlaceholder);
+            //     return {};
+            // }
 
-            element.parent().append('<div class="placeholder" ng-click="setFocusFor(\'' + attrs.ngModel + '\')">{{ placeholder' + attrs.ngModel + ' }}</div>');
+			element.parent().append('<label class="placeholder" ng-click="setFocusFor(\'' + attrs.ngModel + '\')">{{ placeholder' + attrs.ngModel + ' }}</label>');
 
             return function (scope) {
 
-                scope.$parent['placeholder' + attrs.ngModel] = attrs.zbxPlaceholder;
+				scope.$apply(function () {
+					scope['placeholder' + attrs.ngModel] = attrs.zbxPlaceholder;
+				});
 
-                scope.$parent.setFocusFor = function (model) {
-                    angular.element('input[ng-model=' + model + ']').focus();
-                };
+				var input = angular.element('input[ng-model=' + attrs.ngModel + ']'),
+					label = input.closest('div').find('label.placeholder');
 
-                element.on('keyup', function () {
-                    scope.$apply(function () {
-                        scope.$parent['placeholder' + attrs.ngModel] = (element.val() === '') ? attrs.zbxPlaceholder : scope.$parent['placeholder' + attrs.ngModel] = '';
-                    });
+				scope.setFocusFor = function (model) {
+					angular.element('input[ng-model=' + model + ']').focus();
+				};
 
-                }).on('blur', function () {
-                        if (element.val() === '') {
-                            scope.$apply(function () {
-                                scope.$parent['placeholder' + attrs.ngModel] = attrs.zbxPlaceholder;
-                            });
-                        }
-                    });
+				input.on('keyup',function () {
+					scope.$apply(function () {
+						// --Use this if regular placeholder is used (except for IE9)
+						// scope['placeholder' + attrs.ngModel] = (input.val() === '') ? attrs.zbxPlaceholder : scope['placeholder' + attrs.ngModel] = '';
+						// --else
+						if (input.val() === '') {
+							label.removeClass('-selected');
+
+							if (label.html() !== attrs.zbxPlaceholder) {
+								label.html(attrs.zbxPlaceholder);
+							}
+
+						} else {
+							label.addClass('-selected');
+
+							if (label.html().length > 10) {
+								label.html(label.html().substring(0, label.html().indexOf(' ')));
+							}
+
+							// divider was calculated empirically (YMMV) - TT :)
+							var divider = 2;
+
+							if (label.html().length <= 5) {
+								divider = 1.7;
+							}
+
+							input.css('padding-right', label.html().length / divider + 'em');
+						}
+
+					});
+
+				}).on('blur', function () {
+					// --Use this if regular placeholder is used (except for IE9)
+					// if (input.val() === '') {
+					//	scope.$apply(function () {
+					//		scope['placeholder' + attrs.ngModel] = attrs.zbxPlaceholder;
+					//	});
+					// }
+					// --else
+					if (input.val() === '') {
+						label.removeClass('-selected');
+
+						if (label.html() !== attrs.zbxPlaceholder) {
+							label.html(attrs.zbxPlaceholder);
+						}
+					}
+				});
             };
         }
     };
