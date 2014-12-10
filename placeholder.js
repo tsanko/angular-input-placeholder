6997678c0d3ee3a6fdd4f1d43c0a321749777870
Tsanko Tsolov <tsanko.tsolov@gmail.com>
2014-12-10 11:06:49 -0800

    Added more input events.

--- placeholder.js
+++ placeholder.js
@@ -1,38 +1,38 @@
-app.directive('ngPlaceholder', function () {
+app.directive('myPlaceholder', ['$timeout', function ($timeout) {
 	'use strict';
 
         return {
-            restrict: 'A',
-            require: 'ngModel',
-            // If using isolated scope when a sibling directive is used events will be fired into orphaned scope
-            // (i.e. they will not propagate properly).
-            // The real problem here is that this directive modifies DOM outside it's scope.
-            //scope: {},
-            compile: function (element, attrs) {
-				// --Use this if regular placeholder is used (except for IE9)
-                // var i = document.createElement('input');
-                // if ('placeholder' in i) {
-                //     // yay! placeholder is supported - set it!
-                //     element.attr('placeholder', attrs.ngPlaceholder);
-                //     return {};
-                // }
+		restrict: 'A',
+		require: 'ngModel',
+		// If using isolated scope when a sibling directive is used events will be fired into orphaned scope
+		// (i.e. they will not propagate properly).
+		// The real problem here is that this directive modifies DOM outside it's scope.
+		//scope: {},
+		compile: function (element, attrs) {
+			// --Use this if regular placeholder is used (except for IE9)
+	                // var i = document.createElement('input');
+	                // if ('placeholder' in i) {
+	                //     // yay! placeholder is supported - set it!
+	                //     element.attr('placeholder', attrs.ngPlaceholder);
+	                //     return {};
+	                // }
 
-		    element.parent().append('<label class="placeholder" ng-click="setFocusFor(\'' + attrs.ngModel + '\')">{{ placeholder' + attrs.ngModel + ' }}</label>');
+			element.parent().append('<label class="my-placeholder" ng-click="setFocusFor(\'' + attrs.id + '\')"></label>');
 
-            return function (scope) {
+            		return function (scope) {
 
     			function safeApply(scope, fn) {
     				return (scope.$$phase || scope.$root.$$phase) ? fn() : scope.$apply(fn);
     			}
     
-    			var input = angular.element('input[ng-model=' + attrs.ngModel + ']'),
-    			    label = input.closest('div').find('label.placeholder');
+    			var input = element,
+    			    label = input.closest('div').find('label.my-placeholder');
 
-				scope.setFocusFor = function (model) {
-					angular.element('input[ng-model=' + model + ']').focus();
-				};
+			scope.setFocusFor = function (id) {
+				angular.element(document.querySelector('#' + id)).focus();
+			};
 
-				function adjustInput() {
+			function adjustInput() {
 					safeApply(scope, function () {
 						// --Use this if regular placeholder is used (except for IE9)
 						// scope['placeholder' + attrs.ngModel] = (input.val() === '') ? attrs.ngPlaceholder : scope['placeholder' + attrs.ngModel] = '';
@@ -68,36 +68,38 @@ app.directive('ngPlaceholder', function () {
 					});
 				}
 
-				safeApply(scope, function () {
-					scope['placeholder' + attrs.ngModel] = attrs.ngPlaceholder;
-				});
+			safeApply(scope, function () {
+				$timeout(function () {
+					label.html(attrs.vnPlaceholder);
+				}, 0);
+			});
 
-				// In case the input have some default value
-				scope.$watch('attrs.ngModel', function () {
-					adjustInput();
-				});
+			// In case the input have some default value
+			scope.$watch('attrs.ngModel', function () {
+				adjustInput();
+			});
 
-				input
-					.on('keyup', function () {
-						adjustInput();
-					})
-					.on('blur', function () {
-						// --Use this if regular placeholder is used (except for IE9)
-						// if (input.val() === '') {
-						//	scope.$apply(function () {
-						//		scope['placeholder' + attrs.ngModel] = attrs.ngPlaceholder;
-						//	});
-						// }
-						// --else
-						if (input.val() === '') {
-							label.removeClass('-selected');
+			input
+				.on('focus keyup change', function (evt) {
+					adjustInput(evt);
+				})
+				.on('blur', function () {
+					// --Use this if regular placeholder is used (except for IE9)
+					// if (input.val() === '') {
+					//	scope.$apply(function () {
+					//		scope['placeholder' + attrs.ngModel] = attrs.ngPlaceholder;
+					//	});
+					// }
+					// --else
+					if (input.val() === '') {
+						label.removeClass('-selected');
 
-							if (label.html() !== attrs.ngPlaceholder) {
-								label.html(attrs.ngPlaceholder);
-							}
+						if (label.html() !== attrs.myPlaceholder) {
+							label.html(attrs.myPlaceholder);
 						}
-					});
-            };
-        }
-    };
-});
+					}
+				});
+            	};
+        	}
+    	};
+}]);
