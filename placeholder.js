c24fa1de95f74bff7f2cfcf1ffe2e86294eeb52c
Tsanko Tsolov <tsanko.tsolov@gmail.com>
2015-02-14 09:14:40 -0800

    Update placeholder.js

--- placeholder.js
+++ placeholder.js
@@ -9,97 +9,84 @@ app.directive('myPlaceholder', ['$timeout', function ($timeout) {
 		// The real problem here is that this directive modifies DOM outside it's scope.
 		//scope: {},
 		compile: function (element, attrs) {
-			// --Use this if regular placeholder is used (except for IE9)
-	                // var i = document.createElement('input');
-	                // if ('placeholder' in i) {
-	                //     // yay! placeholder is supported - set it!
-	                //     element.attr('placeholder', attrs.ngPlaceholder);
-	                //     return {};
-	                // }
-
+			
 			element.parent().append('<label class="my-placeholder"></label>');
 
             		return function (scope) {
 
-    			function safeApply(scope, fn) {
-    				return (scope.$$phase || scope.$root.$$phase) ? fn() : scope.$apply(fn);
-    			}
-    
-    			var input = element,
-    			    label = input.closest('div').find('label.my-placeholder');
-
-			function adjustInput() {
+	    			function safeApply(scope, fn) {
+	    				return (scope.$$phase || scope.$root.$$phase) ? fn() : scope.$apply(fn);
+	    			}
+	    
+	    			var input = element,
+	    			    label = input.closest('div').find('label.my-placeholder');
+	
+				function adjustInput() {
+					safeApply(scope, function () {
+						// --Use this if regular placeholder is used (except for IE9)
+						// scope['placeholder' + attrs.ngModel] = (input.val() === '') ? attrs.ngPlaceholder : scope['placeholder' + attrs.ngModel] = '';
+						// --else
+						if (evt === undefined && input.val() === '') {
+							label.removeClass('-selected');
+	
+							if (label.html() !== attrs.myPlaceholder) {
+								label.html(attrs.myPlaceholder);
+							}
+	
+						} else {
+							if (!label.hasClass('-selected')) {
+								label.addClass('-selected');
+							}
+	
+							if (attrs.vnPlaceholderShort && attrs.vnPlaceholderShort.length > 0) {
+								label.html(attrs.vnPlaceholderShort);
+							}
+	
+							// --This is needed in case placeholder will slide to right side of the input ***** 
+							// divider was calculated empirically (YMMV) - TT :)
+							//var divider = 2;
+	
+							//if (label.html().length <= 5) {
+							//	divider = 1.7;
+							//}
+	
+							//input.css('padding-right', label.html().length / divider + 'em');
+							// ********************************************************************************
+						}
+	
+					});
+				}
+	
 				safeApply(scope, function () {
-					// --Use this if regular placeholder is used (except for IE9)
-					// scope['placeholder' + attrs.ngModel] = (input.val() === '') ? attrs.ngPlaceholder : scope['placeholder' + attrs.ngModel] = '';
-					// --else
-					if (evt === undefined && input.val() === '') {
-						label.removeClass('-selected');
-
-						if (label.html() !== attrs.myPlaceholder) {
-							label.html(attrs.myPlaceholder);
-						}
-
-					} else {
-						if (!label.hasClass('-selected')) {
-							label.addClass('-selected');
-						}
-
-						if (attrs.vnPlaceholderShort && attrs.vnPlaceholderShort.length > 0) {
-							label.html(attrs.vnPlaceholderShort);
-						}
-
-						// --This is needed in case placeholder will slide to right side of the input ***** 
-						// divider was calculated empirically (YMMV) - TT :)
-						//var divider = 2;
-
-						//if (label.html().length <= 5) {
-						//	divider = 1.7;
-						//}
-
-						//input.css('padding-right', label.html().length / divider + 'em');
-						// ********************************************************************************
-					}
-
+					$timeout(function () {
+						label.html(attrs.vnPlaceholder);
+					}, 0);
 				});
-			}
-
-			safeApply(scope, function () {
-				$timeout(function () {
-					label.html(attrs.vnPlaceholder);
-				}, 0);
-			});
-
-			// In case the input have some default value
-			scope.$watch('attrs.ngModel', function () {
-				adjustInput();
-			});
-			
-			label.on('click', function () {
-				input.focus();
-			});
-
-			input
-				.on('focus keyup change', function (evt) {
-					adjustInput(evt);
-				})
-				.on('blur', function () {
-					// --Use this if regular placeholder is used (except for IE9)
-					// if (input.val() === '') {
-					//	scope.$apply(function () {
-					//		scope['placeholder' + attrs.ngModel] = attrs.ngPlaceholder;
-					//	});
-					// }
-					// --else
-					if (input.val() === '') {
-						label.removeClass('-selected');
-
-						if (label.html() !== attrs.myPlaceholder) {
-							label.html(attrs.myPlaceholder);
-						}
-					}
+	
+				// In case the input have some default value
+				scope.$watch('attrs.ngModel', function () {
+					adjustInput();
 				});
-            	};
+				
+				label
+					.on('click', function () {
+						input.focus();
+					});
+	
+				input
+					.on('focus keyup change', function (evt) {
+						adjustInput(evt);
+					})
+					.on('blur', function () {
+						if (input.val() === '') {
+							label.removeClass('-selected');
+	
+							if (label.html() !== attrs.myPlaceholder) {
+								label.html(attrs.myPlaceholder);
+							}
+						}
+					});
+	            	};
         	}
     	};
 }]);
